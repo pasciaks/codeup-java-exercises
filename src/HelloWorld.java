5e2206815741d40daec0f57e82dd0ad36b11db59
Sheldon Pasciak <sheldon@pasciak.com>
2023-12-12 11:04:41 -0700

    Added comments for completed exercise Bonus

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -16,3 +16,22 @@ public class HelloWorld {
     // Classes need to be separate files, not 'in code' classes.
 
 }
+
+// Bonus (Completed and then removed the created .class file)
+
+//        Build the project from the command line:
+//
+//        Open a terminal and navigate to your project.
+//
+//        Change directories into the src directory. You should see your HelloWorld.java file here.
+//
+//        Run the following command to compile your Java file
+//
+//        javac HelloWorld.java
+//        View the contents of the directory again. You should now see a file named HelloWorld.class (this is the compiled Java bytecode).
+//
+//        Run your program with the following command
+//
+//        java HelloWorld
+//        This process is part of what IntelliJ does when we press the "Run" button. This can start to get very complex very quickly as we start to introduce multiple Java files, multiple packages, and external libraries. For the rest of the course, we will be using IntelliJ to compile and run our applications, but it is good to have an idea of what is going on behind the scenes.
+//
