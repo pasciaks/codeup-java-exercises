8fd01c11decb32b02a4c64863bd6ff9a4e969cf3
Sheldon Pasciak <sheldon@pasciak.com>
2023-12-12 12:40:42 -0700

    Misc

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
 
     // I am a single line comment
@@ -14,6 +16,14 @@ public class Main {
     public static void main(String[] args) {
 
         HelloWorld helloWorld = new HelloWorld();
+
+        System.out.println("Hello, World!");
+
+        System.out.println("args = " + Arrays.toString(args));
+
+        for (String arg : args) {
+            System.out.println("arg = " + arg);
+        }
 
         helloWorld.sayHello();
     }
