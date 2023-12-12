d17e7b3c32a87f9f7677d7fd46204182c0543ad6
Sheldon Pasciak <sheldon@pasciak.com>
2023-12-12 10:39:00 -0700

    Update hello world program

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,12 +4,15 @@ public class HelloWorld {
         System.out.println("Hello, World!");
     }
 
-    //    Follow PascalCase for naming classes.
-    //    Follow camelCase for naming variables and methods.
-    //    Follow SCREAMING_SNAKE_CASE for naming constants.
-    //    Follow snake_case for naming packages.
-    //    Follow kebab-case for naming files.
+    // Follow PascalCase for naming classes.
+    // Follow camelCase for naming variables and methods.
+    // Follow SCREAMING_SNAKE_CASE for naming constants.
+    // Follow snake_case for naming packages.
+    // Follow kebab-case for naming files.
 
-    //    Classes need to be separate files, not 'in code' classes.
+    // Strings need to be in double quotes.
+    // Characters need to be in single quotes.
+
+    // Classes need to be separate files, not 'in code' classes.
 
 }
