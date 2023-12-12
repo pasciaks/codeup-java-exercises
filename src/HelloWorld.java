4da8e90105e2e394ff2c00c7d21385028cf567cc
Sheldon Pasciak <sheldon@pasciak.com>
2023-12-12 10:32:54 -0700

    Add HelloWorld class

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    //    Follow PascalCase for naming classes.
+    //    Follow camelCase for naming variables and methods.
+    //    Follow SCREAMING_SNAKE_CASE for naming constants.
+    //    Follow snake_case for naming packages.
+    //    Follow kebab-case for naming files.
+
+    //    Classes need to be separate files, not 'in code' classes.
+
+}
