1998bec80c06c4238705ada69d45bfb8322f4ca8
Sheldon Pasciak <sheldon@pasciak.com>
2023-12-12 12:56:05 -0700

    Lecture notes

--- src/Main.java
+++ src/Main.java
@@ -26,5 +26,36 @@ public class Main {
         }
 
         helloWorld.sayHello();
+
+        byte myByte = 127;
+        byte myByteInBinary = 0b1111111;
+
+        System.out.println("myByte = " + myByte);
+        System.out.println("myByteInBinary = " + myByteInBinary);
+
+        short myShort = 32767;
+        System.out.println("myShort = " + myShort);
+
+        int myInt = 2147483647;
+        System.out.println("myInt = " + myInt);
+
+        long myLong = 9223372036854775807L;
+        System.out.println("myLong = " + myLong);
+
+        float myFloat = 2.1F;
+        System.out.println("myFloat = " + myFloat);
+
+        double myDouble = 2.1;
+        System.out.println("myDouble = " + myDouble);
+
+        char myChar = 'c';
+        System.out.println("myChar = " + myChar);
+
+        boolean myBoolean = true;
+        System.out.println("myBoolean = " + myBoolean);
+
+        String myString = "Hello, World!";
+        System.out.println("myString = " + myString);
+
     }
 }
