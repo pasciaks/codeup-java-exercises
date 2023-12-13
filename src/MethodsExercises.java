2dd7295a021bd69c2478c399be9a5d91ecff863c
Sheldon Pasciak <sheldon@pasciak.com>
2023-12-13 15:56:12 -0700

    Exercise complete - some bonus complete

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,99 +1,255 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
+
+    public static String sayHello(String name) {
+        return String.format("Hello, %s!", name);
+    }
+
+    public static void sayHello(String greeting, String name) {
+        System.out.printf("%s, %s!\n", greeting, name);
+    }
+
+    public static int returnThree() {
+        return 3;
+    }
+
+    public static String shout(String s) {
+        return s.toUpperCase() + "!!!";
+    }
+
+    public static void count(int n) {
+        if (n <= 0) {
+            System.out.println("All done!");
+            return;
+        }
+        System.out.println(n);
+        count(n - 1);
+    }
+
+    // Factorial using recursion
+    public static long factorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static int addIntegers(int x, int y) {
+        return x + y;
+    }
+
+    public static int subtractIntegers(int x, int y) {
+        return x - y;
+    }
+
+    public static int multiplyIntegers(int x, int y) {
+        return x * y;
+    }
+
+    public static int divideIntegers(int x, int y) {
+        return x / y;
+    }
+
+    public static int modulusIntegers(int x, int y) {
+        return x % y;
+    }
+
+    public static long modulusLongs(long x, long y) {
+        return x % y;
+    }
+
+    public static long addLongs(long x, long y) {
+        return x + y;
+    }
+
+    public static long subtractLongs(long x, long y) {
+        return x - y;
+    }
+
+    public static long multiplyLongs(long x, long y) {
+        return x * y;
+    }
+
+    public static long divideLongs(long x, long y) {
+        return x / y;
+    }
+
+    public static int productFromValueAndNumber(int value, int number) {
+        int product = 0;
+        for (int i = 0; i < number; i++) {
+            product += value;
+        }
+        return product;
+    }
+
+    public static int getInteger(int min, int max) {
+        Scanner scanner;
+        boolean shouldTryAgain = true;
+        int validUserInput = 0;
+        do {
+            scanner = new Scanner(System.in);
+            System.out.printf("Enter a number between %d and %d: ", min, max);
+            String userInputString = scanner.nextLine();
+            int userInput = Integer.parseInt(userInputString);
+            if (userInput >= min && userInput <= max) {
+                validUserInput = userInput;
+                shouldTryAgain = false;
+            }
+        } while (shouldTryAgain);
+        return validUserInput;
+    }
 
     public static void main(String[] args) {
 
-    //        Exercises
-    //        Create a class named MethodsExercises. Inside of your class, write code to create the specified methods. Test your code by creating a main method and calling each of the methods you've created.
-    //
-    //        Basic Arithmetic
-    //
-    //        Create four separate methods. Each will perform an arithmetic operation:
-    //
-    //        Addition
-    //                Subtraction
-    //        Multiplication
-    //                Division
-    //        Each function needs to take two parameters/arguments so that the operation can be performed.
-    //
-    //                Test your methods and verify the output.
-    //
-    //        Add a modulus method that finds the modulus of two numbers.
-    //
-    //                Food for thought: What happens if we try to divide by zero? What should happen?
-    //
-    //                Bonus
-    //
-    //        Create your multiplication method without the * operator (Hint: a loop might be helpful).
-    //        Do the above with recursion.
-    //                Create a method that validates that user input is in a certain range and returns that input as an integer if it is within the given range. If not, prompt the user to input their number again until the input is within range.
-    //
-    //                The method signature should look like this:
-    //
-    //        public static int getInteger(int min, int max);
-    //        and is used like this:
-    //
-    //        System.out.print("Enter a number between 1 and 10: ");
-    //        int userInput = getInteger(1, 10);
-    //        If the input is invalid, prompt the user again.
-    //
-    //                Hint: recursion might be helpful here!
-    //
-    //                Calculate the factorial of a number.
-    //
-    //                Prompt the user to enter an integer from 1 to 10.
-    //
-    //        Display the factorial of the number entered by the user.
-    //
-    //        Ask if the user wants to continue.
-    //
-    //        Use a for loop to calculate the factorial.
-    //
-    //                Assume that the user will enter an integer, but verify it’s between 1 and
-    //
-    //        Use the long type to store the factorial.
-    //
-    //        Continue only if the user agrees to.
-    //
-    //        A factorial is a number multiplied by each of the numbers before it.
-    //
-    //                Factorials are denoted by the exclamation point (n!). Ex:
-    //
-    //        1! = 1               = 1
-    //        2! = 1 x 2           = 2
-    //        3! = 1 x 2 x 3       = 6
-    //        4! = 1 x 2 x 3 x 4   = 24
-    //        Bonus
-    //
-    //        Test the application and find the integer for the highest factorial that can be accurately calculated by this application, then modify the prompt so that it prompts the user for a number "from 1 to {the highest integer that returns accurate factorial calculation}". Don’t forget to change your verification too!
-    //                Use recursion to implement the factorial.
-    //                Create an application that simulates dice rolling.
-    //
-    //        Ask the user to enter the number of sides for a pair of dice.
-    //        Prompt the user to roll the dice.
-    //                "Roll" two n-sided dice, display the results of each, and then ask the user if he/she wants to roll the dice again.
-    //        Use static methods to implement the method(s) that generate the random numbers.
-    //                Use the .random method of the java.lang.Math class to generate random numbers.
-    //        Game Development 101
-    //
-    //        Welcome to the world of game development!
-    //
-    //                You are going to build a high-low guessing game. Create a class named HighLow inside of src.
-    //
-    //        The specs for the game are:
-    //
-    //        Game picks a random number between 1 and 100.
-    //        Prompts user to guess the number.
-    //        All user inputs are validated.
-    //                If user's guess is less than the number, it outputs "HIGHER".
-    //        If user's guess is more than the number, it outputs "LOWER".
-    //        If a user guesses the number, the game should declare "GOOD GUESS!"
-    //        Hints
-    //
-    //        Use the random method of the java.lang.Math class to generate a random number.
-    //        Bonus
-    //
-    //        Keep track of how many guesses a user makes.
-    //                Set an upper limit on the number of guesses.
+        //        Exercises
+        //        Create a class named MethodsExercises. Inside of your class, write code to create the specified methods.
+        //
+        //        Test your code by creating a main method and calling each of the methods you've created.
+        //
+        //        Basic Arithmetic
+        //
+        //        Create four separate methods. Each will perform an arithmetic operation:
+
+        //        Addition
+
+        int a = 5;
+        int b = 10;
+        System.out.printf("%d + %d = %d\n", a, b, addIntegers(a, b));
+
+        //        Subtraction
+
+        int c = 20;
+        int d = 5;
+        System.out.printf("%d - %d = %d\n", c, d, subtractIntegers(c, d));
+
+        //        Multiplication
+
+        int e = 5;
+        int f = 5;
+        System.out.printf("%d * %d = %d\n", e, f, multiplyIntegers(e, f));
+
+        //        Division
+
+        int g = 10;
+        int h = 2;
+        System.out.printf("%d / %d = %d\n", g, h, divideIntegers(g, h));
+
+        //        Each function needs to take two parameters/arguments so that the operation can be performed.
+        //
+        //                Test your methods and verify the output.
+
+        //        Add a modulus method that finds the modulus of two numbers.
+
+        int i = 10;
+        int j = 3;
+        System.out.printf("%d %% %d = %d\n", i, j, modulusIntegers(i, j));
+
+        //                Food for thought: What happens if we try to divide by zero? What should happen? Error/Exception
+
+        int k = 10;
+        int l = 0;
+
+        try {
+
+            System.out.printf("%d / %d = %d\n", k, l, divideIntegers(k, l));
+
+        } catch (Exception exception) {
+
+            System.out.println("Exception caught: " + exception);
+
+            //             Exception in thread "main" java.lang.ArithmeticException: divide by zero
+            //             at MethodsExercises.divideIntegers(MethodsExercises.java:47)
+            //             at MethodsExercises.main(MethodsExercises.java:124)
+
+        }
+
+
+        //                Bonus
+
+        //        Create your multiplication method without the * operator (Hint: a loop might be helpful).
+
+        int m = 5;
+        int n = 3;
+        System.out.printf("%d * %d = %d\n", m, n, productFromValueAndNumber(m, n));
+
+        //        Do the above with recursion.
+
+        //        Create a method that validates that user input is in a certain range and returns that input as an integer if it is within the given range. If not, prompt the user to input their number again until the input is within range.
+
+        //                The method signature should look like this:
+        //
+        //        public static int getInteger(int min, int max);
+
+        //        and is used like this:
+        //
+        //        System.out.print("Enter a number between 1 and 10: ");
+
+        int userInput = getInteger(1, 10);
+        System.out.printf("You entered: %d\n", userInput);
+
+        //        If the input is invalid, prompt the user again.
+        //
+        //                Hint: recursion might be helpful here!
+        //
+        //                Calculate the factorial of a number.
+        //
+        //                Prompt the user to enter an integer from 1 to 10.
+        //
+        //        Display the factorial of the number entered by the user.
+        //
+        //        Ask if the user wants to continue.
+        //
+        //        Use a for loop to calculate the factorial.
+        //
+        //                Assume that the user will enter an integer, but verify it’s between 1 and
+        //
+        //        Use the long type to store the factorial.
+        //
+        //        Continue only if the user agrees to.
+        //
+        //        A factorial is a number multiplied by each of the numbers before it.
+        //
+        //                Factorials are denoted by the exclamation point (n!). Ex:
+        //
+        //        1! = 1               = 1
+        //        2! = 1 x 2           = 2
+        //        3! = 1 x 2 x 3       = 6
+        //        4! = 1 x 2 x 3 x 4   = 24
+
+        //        Bonus
+        //
+        //        Test the application and find the integer for the highest factorial that can be accurately calculated by this application, then modify the prompt so that it prompts the user for a number "from 1 to {the highest integer that returns accurate factorial calculation}". Don’t forget to change your verification too!
+        //                Use recursion to implement the factorial.
+        //                Create an application that simulates dice rolling.
+        //
+        //        Ask the user to enter the number of sides for a pair of dice.
+        //        Prompt the user to roll the dice.
+        //                "Roll" two n-sided dice, display the results of each, and then ask the user if he/she wants to roll the dice again.
+        //        Use static methods to implement the method(s) that generate the random numbers.
+        //                Use the .random method of the java.lang.Math class to generate random numbers.
+        //        Game Development 101
+        //
+        //        Welcome to the world of game development!
+        //
+        //                You are going to build a high-low guessing game. Create a class named HighLow inside of src.
+        //
+        //        The specs for the game are:
+        //
+        //        Game picks a random number between 1 and 100.
+        //        Prompts user to guess the number.
+        //        All user inputs are validated.
+        //                If user's guess is less than the number, it outputs "HIGHER".
+        //        If user's guess is more than the number, it outputs "LOWER".
+        //        If a user guesses the number, the game should declare "GOOD GUESS!"
+        //        Hints
+        //
+        //        Use the random method of the java.lang.Math class to generate a random number.
+
+        //        Bonus
+        //
+        //        Keep track of how many guesses a user makes.
+        //                Set an upper limit on the number of guesses.
 
 
     }
