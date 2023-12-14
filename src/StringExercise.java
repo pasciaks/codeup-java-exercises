11a5e3f54ede7257226a2ac121c8f0383c766a72
Sheldon Pasciak <sheldon@pasciak.com>
2023-12-14 07:34:35 -0700

    Misc code cleanup

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -8,35 +8,29 @@ public class StringExercise {
         // Create a class named StringExercise with a main method.
         //
         // For each of the following output examples: create a String variable that contains the desired output and print it out to the console, you can do this with only one String variable and one print statement for each output example.
-        //
+
+
         // We don't need no education
         // We don't need no thought control
 
-        String lineOne = "We don't need no education";
+        String lineOne = "We don't need no education\nWe don't need no thought control";
         System.out.println(lineOne);
-
-        String lineTwo = "We don't need no thought control";
-        System.out.println(lineTwo);
 
         // Check "this" out!, "s inside of "s!
 
-        String lineThree = "Check \"this\" out!, \"s inside of \"s!";
-        System.out.println(lineThree);
+        String lineTwo = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(lineTwo);
 
         // In windows, the main drive is usually C:\
 
-        String lineFour = "In windows, the main drive is usually C:\\";
-        System.out.println(lineFour);
+        String lineThree = "In windows, the main drive is usually C:\\";
+        System.out.println(lineThree);
 
         // I can do backslashes \, double backslashes \\,
-
-        String lineFive = "I can do backslashes \\, double backslashes \\\\,";
-        System.out.println(lineFive);
-
         // and the amazing triple backslash \\\!
 
-        String lineSix = "and the amazing triple backslash \\\\\\!";
-        System.out.println(lineSix);
+        String lineFour = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+        System.out.println(lineFour);
 
     }
 }
