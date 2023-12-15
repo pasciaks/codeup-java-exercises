10d1ea194f1e99814e3825c14107c38732ad1fea
Sheldon Pasciak <sheldon@pasciak.com>
2023-12-15 08:32:46 -0700

    More exercise practice

---
+++ src/ExamplePractice.java
@@ -0,0 +1,19 @@
+public class ExamplePractice {
+
+    public static void main(String[] args) {
+
+        String testString ="We don't need no education\n" +
+                "We don't need no thought control";
+        System.out.println(testString);
+
+        String testString2 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(testString2);
+
+        String testString3 = "In windows, the main drive is usually C:\\";
+        System.out.println(testString3);
+
+        String testString4 = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(testString4);
+    }
+}
