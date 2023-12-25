caff4a0237432967a0bfbcf4f8ef09c32780ccbb
Sheldon Pasciak <sheldon@pasciak.com>
2023-12-25 09:29:03 -0500

    Format code

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -38,7 +38,7 @@ public class MethodsExercises {
 
     // Factorial using recursion
     public static long factorial(int n) {
-        if (n == 1 || n ==0) {
+        if (n == 1 || n == 0) {
             return 1;
         }
         return n * factorial(n - 1);
